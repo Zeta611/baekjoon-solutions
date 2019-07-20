ae315e89421b4ff455dc0cab9a0fba055a0d4e09
Jaeho Lee <jaeho.lee@snu.ac.kr>
2019-07-21 01:32:49 +0900

    :sparkles: Added 2588

---
+++ 2588 곱셈.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = input()
+c, d, e = map(int, b)
+print(a * e)
+print(a * d)
+print(a * c)
+print(a * int(b))
+
+# Short coding
+a,b=int(input()),input()
+c,d,e=map(int,b)
+print(a*e,a*d,a*c,a*int(b),sep='\n')
