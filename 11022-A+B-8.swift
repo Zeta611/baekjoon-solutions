9b3a2a60ba5caa453c55db8efd3fab4a4542b3bb
Jaeho Lee <jaeho.lee@snu.ac.kr>
2019-08-04 19:36:20 +0900

    :sparkles: Added solution for 11022

---
+++ 11022-A+B-8.swift
@@ -0,0 +1,12 @@
+let numberOfCases = Int(readLine()!)!
+for i in 1...numberOfCases {
+  print("Case #\(i): ", terminator: "")
+  let input = readLine()!
+    .split(separator: " ")
+  print(input.joined(separator: " + "), "=", terminator: " ")
+  print(input
+    .map{ Int($0)! }
+    .reduce(0, +))
+}
+// for i in 1...Int(readLine()!)!{let a=readLine()!.split{$0==" "}.map{Int($0)!}
+// print("Case #\(i): \(a[0]) + \(a[1]) = \(a[0]+a[1])")}
