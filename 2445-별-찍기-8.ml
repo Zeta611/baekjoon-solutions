a9af744ece32bba853ef11a050a0c688c7799224
Jaeho Lee <jaeho.lee@snu.ac.kr>
2019-10-31 04:16:13 +0900

    :sparkles: Added a solution for 2445

---
+++ 2445-별-찍기-8.ml
@@ -0,0 +1,31 @@
+let print_pyramid n =
+  let rec print_spaces = function
+    | 0 -> ()
+    | n -> print_string " "; print_spaces (n - 1)
+  in
+  let rec print_stars = function
+    | 0 -> ()
+    | n -> print_string "*"; print_stars (n - 1)
+  in
+  let rec print_pyramid_top = function
+    | 0 -> ()
+    | m ->
+      print_stars (n - m + 1);
+      print_spaces (2 * (m - 1));
+      print_stars (n - m + 1);
+      print_endline "";
+      print_pyramid_top (m - 1)
+  in
+  let rec print_pyramid_bottom = function
+    | 0 -> ()
+    | m ->
+      print_stars m;
+      print_spaces (2 * (n - m));
+      print_stars m;
+      print_endline "";
+      print_pyramid_bottom (m - 1)
+  in
+  print_pyramid_top n;
+  print_pyramid_bottom (n - 1)
+
+let _ = print_pyramid (read_int ())
