d1455fd04c4f2b57d73a21a4c6051b7cd5a6751e
Jaeho Lee <jaeho.lee@snu.ac.kr>
2020-05-21 10:03:01 +0900

    :art: Refactor 10870

--- 10870-피보나치-수-5.rs
+++ 10870-피보나치-수-5.rs
@@ -1,9 +1,5 @@
 fn fibonacci(n: i32) -> i32 {
-    if n <= 1 {
-        return n;
-    } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
+    if n <= 1 { n } else { fibonacci(n - 1) + fibonacci(n - 2) }
 }
 
 fn main() {
