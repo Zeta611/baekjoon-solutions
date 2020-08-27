0af368719991d6a311e0a75b8d1d6b332ca16d0e
Jaeho Lee <jaeho.lee@snu.ac.kr>
2020-08-27 13:41:21 +0000

    :zap: Optimize space complexity of 12865
    from O(n^2) to O(n).

--- 12865-평범한-배낭.c
+++ 12865-평범한-배낭.c
@@ -8,40 +8,24 @@ static inline int max(int a, int b)
 	return a > b ? a : b;
 }
 
-static inline int get(int *opt, int i, int w)
-{
-	return opt[i * K_MAX + w];
-}
-
-static inline void set(int *opt, int i, int w, int v)
-{
-	opt[i * K_MAX + w] = v;
-}
-
 int main(void)
 {
 	int n, k;
 	scanf("%d %d", &n, &k);
 
-	int weight[N_MAX], value[N_MAX];
+	int *opt = malloc(N_MAX * K_MAX * sizeof(int));
+	opt[0] = 0;
 	for (int i = 1; i <= n; ++i) {
-		scanf("%d %d", &weight[i], &value[i]);
-	}
+		int wi, vi;
+		scanf("%d %d", &wi, &vi);
 
-	int *opt = calloc(N_MAX * K_MAX, sizeof(int));
-	for (int i = 1; i <= n; ++i) {
-		for (int w = 1; w <= k; ++w) {
-			int v = get(opt, i - 1, w);
-			int wi = weight[i];
-			int vi = value[i];
-			if (wi <= w) {
-				v = max(v, get(opt, i - 1, w - wi) + vi);
-			}
-			set(opt, i, w, v);
+		for (int w = k; w >= 1; --w) {
+			int v = opt[w];
+			opt[w] = wi > w ? v : max(v, opt[w - wi] + vi);
 		}
 	}
 
-	printf("%d\n", get(opt, n, k));
+	printf("%d\n", opt[k]);
 	free(opt);
 
 	return 0;
