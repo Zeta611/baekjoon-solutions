fdffdcde733107c42ad59abaac42e76284ae5b4b
Jaeho Lee <jaeho.lee@snu.ac.kr>
2021-08-29 09:40:56 +0000

    :recycle: Add a more straightforward solution to 6549

--- 6549-히스토그램에서-가장-큰-직사각형.cpp
+++ 6549-히스토그램에서-가장-큰-직사각형.cpp
@@ -4,46 +4,56 @@
 #include <stack>
 #include <vector>
 
-long long max_rect(const std::vector<int>& hist)
+long long largest_rect(const std::vector<int> &histogram)
 {
-    long long ret{0};
-    std::stack<std::vector<int>::const_iterator> stack;
-    auto it = begin(hist);
-    while (it != end(hist) || !stack.empty()) {
-        // Invar1: `stack` is monotonically increasing.
-        // Invar2: `stack[i]` is shorter than all bars after `stack[i-1]`.
-        if (it != end(hist) && (stack.empty() || *stack.top() <= *it)) {
-            stack.push(it++);
-        } else {
-            // `it` is the exclusive right barrier (invar1)
-            auto top_it = stack.top();
-            stack.pop();
-            // `left_barr` is the inclusive left barrier (invar2)
-            auto left_barr = stack.empty() ? begin(hist) : next(stack.top());
-            long long area = std::distance(left_barr, it) * *top_it;
-            ret = std::max(ret, area);
+    long long max{0};
+    std::stack<int> left_bounds;
+
+    for (int i{0}; i < histogram.size(); ++i) {
+        while (!left_bounds.empty() &&
+               histogram[left_bounds.top()] > histogram[i]) {
+            const int h{histogram[left_bounds.top()]};
+            left_bounds.pop();
+
+            max = std::max(
+                max,
+                h * static_cast<long long>(
+                        i - (left_bounds.empty() ? 0 : left_bounds.top() + 1)));
         }
+        left_bounds.push(i);
     }
-    return ret;
+
+    while (!left_bounds.empty()) {
+        const int h{histogram[left_bounds.top()]};
+        left_bounds.pop();
+
+        max = std::max(
+            max, h * static_cast<long long>(
+                         histogram.size() -
+                         (left_bounds.empty() ? 0 : left_bounds.top() + 1)));
+    }
+
+    return max;
 }
 
 int main()
 {
     std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+    std::cin.tie(nullptr);
 
-    std::vector<int> hist;
-    hist.reserve(10'000);
+    std::vector<int> histogram;
     while (true) {
+        histogram.clear();
+
         int n;
         std::cin >> n;
-        if (n == 0) {
+        if (!n) {
             break;
         }
-        std::copy_n(
-            std::istream_iterator<int>(std::cin), n, back_inserter(hist));
-        std::cout << max_rect(hist) << '\n';
-        hist.clear();
+
+        copy_n(std::istream_iterator<int>(std::cin), n,
+               back_inserter(histogram));
+
+        std::cout << largest_rect(histogram) << '\n';
     }
-    return 0;
 }
