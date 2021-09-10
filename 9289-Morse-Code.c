20eb763c2dd87c8484930815fb343194ea083eac
Jaeho Lee <jaeho.lee@snu.ac.kr>
2021-09-10 13:48:57 +0000

    :sparkles: Add a solution to 9289

---
+++ 9289-Morse-Code.c
@@ -0,0 +1,34 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+char codes[][5] = {".-",   "-...", "-.-.", "-..",  ".",    "..-.", "--.",
+                   "....", "..",   ".---", "-.-",  ".-..", "--",   "-.",
+                   "---",  ".--.", "--.-", ".-.",  "...",  "-",    "..-",
+                   "...-", ".--",  "-..-", "-.--", "--.."};
+
+char buf[5];
+char find()
+{
+        for (char c = 'A'; c <= 'Z'; ++c) {
+                if (!strcmp(codes[c - 'A'], buf)) {
+                        return c;
+                }
+        }
+        return '\0';
+}
+
+int main(void)
+{
+        int n;
+        scanf("%d", &n);
+
+        for (int i = 1; i <= n; ++i) {
+                printf("Case %d: ", i);
+                for (int j = 0; j < 5; ++j) {
+                        scanf("%s", buf);
+                        putchar(find());
+                }
+                putchar('\n');
+        }
+}
